0b62e9a6a0dfa31cc72816b182774a9fbf41033e
artamono <44265301+artamono@users.noreply.github.com>
2021-03-27 23:41:05 +0700

    Update plots.py
    added dendrogram

--- druhg/plots.py
+++ druhg/plots.py
@@ -10,6 +10,228 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from warnings import warn
 
+class SingleLinkage(object):
+# todo: make it pretty
+    def __init__(self, mst_pairs, values, labels):
+        self._mst_pairs = mst_pairs
+        self._values = values
+        # self._data = data
+        self._labels = labels
+
+    def fast_find(self, unionfind, n):
+        n = int(n)
+        p = unionfind[n]
+        if p == 0:
+            return n
+
+        while unionfind[p] != 0:
+            p = unionfind[p]
+
+        # label up to the root
+        while p != n:
+            temp = unionfind[n]
+            unionfind[n] = p
+            n = temp
+
+        return p
+
+
+    def draw_dendrogram(self, ax, pairs, values, labels, lw=20., alpha=0.4, cmap='viridis'):
+        try:
+            from matplotlib import collections as mc
+            from matplotlib.pyplot import Arrow
+        except ImportError:
+            raise ImportError('You must install the matplotlib library to plot the minimum spanning tree.')
+
+        min_index, max_index = min(pairs), max(pairs)
+        if min_index < 0:
+            raise ValueError('Indices should be non-negative')
+
+        size = int(len(pairs) / 2 + 1)
+
+        union_size = size
+        if max_index > union_size - 1:
+            union_size = max_index + 1
+        union_size += 2
+
+        # we will create Union Find as usual
+        uf, sz = np.zeros(2 * union_size, dtype=int), np.ones(union_size)
+        next_label = union_size + 1
+        # also we need links
+        l, r = np.arange(0, 2*union_size), np.arange(0, 2*union_size)
+
+        next_label = union_size + 1
+        for j in range(0, size - 1):
+            a, b = pairs[2 * j], pairs[2 * j + 1]
+
+            # we will stack first cluster on the left of second
+            aa = a
+            while aa != r[aa]:
+                aa = r[aa]
+            bb = b
+            while bb != l[bb]:
+                bb = l[bb]
+            l[bb] = aa
+            r[aa] = bb # linking
+
+
+            aa = a
+            while aa != l[aa]:
+                aa = l[aa]
+            bb = b
+            while bb != r[bb]:
+                bb = r[bb]
+            l[next_label] = aa # marking the borders
+            r[next_label] = bb
+
+            aa, bb = self.fast_find(uf, a), self.fast_find(uf, b)
+            uf[aa] = uf[bb] = next_label
+
+            # i = next_label - union_size
+            # a2 = (uf[a] != 0) * (aa - union_size)
+            # b2 = (uf[b] != 0) * (bb - union_size)
+            # na, nb = sz[a2], sz[b2]
+            # sz[i] = na + nb
+
+            next_label += 1
+
+        x_arr = self.arrange_nodes_on_x_axis(uf, union_size, l, r, 200.)
+
+        norm = len(np.unique(pairs))
+        sm = cm.ScalarMappable(cmap=cmap,
+                                   norm=Normalize(0, norm))
+        sm.set_array(norm)
+
+        colors = self.get_dendro_colors(labels)
+        heights = {}
+        uf.fill(0)
+        next_label = union_size + 1
+        for j in range(0, size - 1):
+            v = np.log2(1. + values[j]) # logarithm
+            heights[next_label] = v
+
+            a, b = pairs[2 * j], pairs[2 * j + 1]
+
+            # i = next_label - union_size
+            aa, bb = self.fast_find(uf, a), self.fast_find(uf, b)
+            x_arr[next_label] = (x_arr[r[aa]] + x_arr[l[bb]])/2.
+            uf[aa] = uf[bb] = next_label
+            next_label += 1
+
+            # a = (uf[a] != 0) * (aa - union_size)
+            # b = (uf[b] != 0) * (bb - union_size)
+            # na, nb = sz[a], sz[b]
+            # sz[i] = na + nb
+
+            ha, hb = 0, 0
+            xa, xb = x_arr[aa], x_arr[bb]
+            if aa in heights:
+                ha = heights[aa]
+            if bb in heights:
+                hb = heights[bb]
+
+            c = 'gray'
+            if labels[a] == labels[b] and labels[a] > 0:
+                c = colors[labels[a]]
+
+            ax.plot([xa, xa], [ha, v], color=c)
+            ax.plot([xb, xb], [hb, v], color=c)
+            ax.plot([xa, xb], [v, v], color=c)
+
+        ax.set_xticks([])
+        for side in ('right', 'top', 'bottom'):
+            ax.spines[side].set_visible(False)
+        ax.set_ylabel('distance')
+
+        # line_collection.set_array(self._mst[:, 2].T)
+        return ax
+
+    def arrange_nodes_on_x_axis(self, uf, union_size, l, r, step = 2.):
+        x_arr = np.zeros(2 * union_size, dtype=int)
+        processed = {}
+        x = 1.
+        # constructing the tree
+        # there is a possibility of a forest instead of a tree
+        for a in (0, union_size):
+            if l[a] == a: # can happened when some indices were not passed
+                continue
+            aa = self.fast_find(uf, a)
+            if aa in processed:
+                continue
+            processed[aa] = aa
+            aa = a
+            while aa != r[aa]:
+                aa = r[aa]
+            while aa != l[aa]:
+                x_arr[aa] = x
+                x += step
+                aa = l[aa]
+            x_arr[aa] = x
+
+        return x_arr
+
+    def get_dendro_colors(self, labels):
+        try:
+            import seaborn as sns
+        except ImportError:
+            raise ImportError('You must install the seaborn library to draw colored labels.')
+
+        unique, counts = np.unique(labels, return_counts=True)
+        sorteds = np.argsort(counts)
+        s = len(sorteds)
+
+        i = sorteds[s - 1]
+        max_size = counts[i]
+        if unique[i] < 0:
+            max_size = counts[sorteds[s - 2]]
+
+        color_map = {}
+        palette = sns.color_palette('bright', s + 1)
+        col = 0
+        a = (1. - 0.3) / (max_size - 1)
+        b = 0.3 - a
+        while s:
+            s -= 1
+            i = sorteds[s]
+            if unique[i] < 0:  # outliers
+                color_map[unique[i]] = (0., 0., 0., 0.15)
+                continue
+            alpha = a * counts[i] + b
+            color_map[unique[i]] = palette[col] + (alpha,)
+            col += 1
+
+        return color_map
+
+    def plot(self, axis=None):
+        """Plot the dendrogram.
+
+        Parameters
+        ----------
+
+        axis : matplotlib axis, optional
+               The axis to render the plot to
+
+        Returns
+        -------
+
+        axis : matplotlib axis
+                The axis used the render the plot.
+        """
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            raise ImportError('You must install the matplotlib library to plot the minimum spanning tree.')
+
+        if axis is None:
+            axis = plt.gca()
+            axis.set_axis_off()
+
+
+        axis = self.draw_dendrogram(axis, self._mst_pairs, self._values, self._labels)
+
+        return axis
+
+
 class MinimumSpanningTree(object):
     def __init__(self, mst_pairs, data, labels):
         self._mst_pairs = mst_pairs
@@ -208,7 +430,7 @@ class MinimumSpanningTree(object):
             raise ImportError('You must install the matplotlib library to plot the minimum spanning tree.')
 
         if self._data.shape[0] > 32767:
-            warn('Too many data points for safe rendering of an minimal spanning tree!')
+            warn('Too many data points for safe rendering of a minimal spanning tree!')
             return None
 
         if axis is None:
